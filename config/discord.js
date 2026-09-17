c6bf01e8fda479512188a0171448d5c862010ca7
Rikardo66 <nemethkovacsrichard@gmail.com>
2026-09-17 15:13:20 +0200

    Maybe cache

--- config/discord.js
+++ config/discord.js
@@ -1,6 +1,6 @@
 (async () => {
     try {
-        const module = await import("./vendor/discord-sdk.js?v=50");
+        const module = await import("./vendor/discord-sdk.js?v=55");
         window.DiscordSDK = module.DiscordSDK;
         console.log("Discord SDK loaded");
     } catch (error) {
